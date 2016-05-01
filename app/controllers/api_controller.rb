d2f595b337396596a54934686db533d7c6fa11a5
Beth Haubert <bethany.haubert@gmail.com>
2016-05-01 18:39:29 -0500

    updated api output

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,6 @@ class ApiController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-    render json: Cat.select("id, name, photo")
+    render json: Cat.select("cats.id, cats.name, cats.photo").includes(:favorites).as_json(:include => {:favorites => {:only => [:thing]}})
   end
 end
